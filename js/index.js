743bdffed12ced777b50cbf6b221c1994b7a74e1
muroran <joho@city.muroran.lg.jp>
2016-11-15 10:22:22 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // 地図表示時の中心座標
-var init_center_coords = [140.1112972, 35.6129448];
+var init_center_coords = [140.99443361337762, 42.32790062660304];
 
 // Bing APIのキー
 var bing_api_key = 'AhGQykUKW2-u1PwVjLwQkSA_1rCTFESEC7bCZ0MBrnzVbVy7KBHsmLgwW_iRJg17';
